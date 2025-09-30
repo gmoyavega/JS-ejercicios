0da20f5c3f172fbcbbf656b7daf4248f3fb8a996
Gabriela Moya Vega <gmoyavega06@gmail.com>
2025-09-30 14:24:44 -0300

    Delete jsEntradaSalida-5.js

--- jsEntradaSalida-5.js
+++
@@ -1,5 +0,0 @@
-function Mostar(){
-    let nombre = document.getElementById('elNombre').value 
-    let edad = document.getElementById('laEdad').value
-    alert('usted se llama ' + nombre + ' y tiene ' + edad + ' años')
-}
