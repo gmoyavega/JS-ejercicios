24d9dc771d64cae5f79f944b7816d0d35a48dfbe
Gabriela Moya Vega <gmoyavega06@gmail.com>
2025-09-30 14:25:15 -0300

    Delete jsEntradaSalida-8.js

--- jsEntradaSalida-8.js
+++
@@ -1,5 +0,0 @@
-function SacarResto(){
-    let dividendo = document.getElementById('numeroDividendo').value 
-    let divisor = document.getElementById('numeroDivisor').value 
-    alert(parseInt(dividendo) % parseInt(divisor))
-}
