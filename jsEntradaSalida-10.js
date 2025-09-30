13d52972c269ced69f64d7e70000d247c604dae9
Gabriela Moya Vega <gmoyavega06@gmail.com>
2025-09-30 14:24:00 -0300

    Delete jsEntradaSalida-10.js

--- jsEntradaSalida-10.js
+++
@@ -1,5 +0,0 @@
-function MostrarAumento(){
-    let importe = document.getElementById('importe').value 
-    let descuento = parseInt(importe) - (importe * 0.25)
-    document.getElementById('resultado').value = descuento
-}
