f98b4f5d53b07d2267b378fea61288410e311ce9
Gabriela Moya Vega <gmoyavega06@gmail.com>
2025-09-30 14:25:25 -0300

    Delete jsEntradaSalida-9.js

--- jsEntradaSalida-9.js
+++
@@ -1,5 +0,0 @@
-function MostrarAumento(){
-    let sueldo = document.getElementById('sueldo').value 
-    let resultado = parseInt(sueldo) + (sueldo * 0,100 )
-    document.getElementById('resultado').value=resultado
-}
