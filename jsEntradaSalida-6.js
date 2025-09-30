20059c3da04dd8a558827ce65d99fe22e2bf02d3
Gabriela Moya Vega <gmoyavega06@gmail.com>
2025-09-30 14:24:53 -0300

    Delete jsEntradaSalida-6.js

--- jsEntradaSalida-6.js
+++
@@ -1,5 +0,0 @@
-function sumar(){
-    let numero1 = document.getElementById('numeroUno').value 
-    let numero2 = document.getElementById('numeroDos').value 
-    alert(parseInt(numero1)+ parseInt(numero2));
-}
